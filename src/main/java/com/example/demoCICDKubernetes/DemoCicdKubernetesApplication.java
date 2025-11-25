e2eb79185092060aee0e8e88841b3d832c398c0b
tonmoym83 <contacttonmoy@gmail.com>
2025-11-25 22:24:05 +0000

    Initial commit

---
+++ src/main/java/com/example/demoCICDKubernetes/DemoCicdKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoCICDKubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCicdKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCicdKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoCICDKubernetes/DemoCicdKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoCICDKubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCicdKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
